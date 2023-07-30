9ddfce8ae1c5cafb53744731056dacb49538710f
Celso Taliatelli <celsotaliatelli@hotmail.com>
2023-07-30 18:44:03 -0300

    env: criando repositories e configurando sequelize

---
+++ src/domain/repository/product-interface.ts
@@ -0,0 +1,6 @@
+import Product from "../entity/product";
+import RespositoryInterface from "./repository-interface";
+
+export default interface ProductRepositoryInterface  extends RespositoryInterface<Product> {
+
+}
