56af77de7225557edb3419adf461018f8e19479d
Celso Taliatelli <celsotaliatelli@hotmail.com>
2023-09-25 17:18:10 -0300

    criando teste e2e

---
+++ src/infra/api/__tests__/customer.e2e.spec.ts
@@ -0,0 +1,29 @@
+import {app, sequelize}  from "../express";
+import request from "supertest";
+describe("E2E test for customer", () => {
+
+    beforeEach(async ()=> {
+        await sequelize.sync({force: true});
+    });
+
+    afterAll(async () => {
+        await sequelize.close();
+    });
+
+    it("Should create a customer", async () => {
+        const response = await request(app)
+            .post("/customer")
+            .send({
+                name:"Jonh",
+                address: {
+                    street:"Street",
+                    city: "City",
+                    number:123,
+                    zip:"123456"
+                },
+            });
+
+            expect(response.status).toBe(200);
+            expect(response.body.name).toBe("Jonh")
+    })
+})
