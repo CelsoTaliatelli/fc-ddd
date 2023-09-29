c88cf00ebf4e38079c6c6639c7fe8eb4e2ff2905
Celso Taliatelli <celsotaliatelli@hotmail.com>
2023-09-29 13:52:09 -0300

    add implemtação presenters to product

--- src/infra/api/__tests__/product.e2e.spec.ts
+++ src/infra/api/__tests__/product.e2e.spec.ts
@@ -29,4 +29,33 @@ describe("E2E test for product", () => {
             expect(response.body.products[0].id).toBe("123")
             expect(response.body.products[1].id).toBe("567")
     });
+
+    it("Should list products in json and xml", async () => {
+
+        const product = new Product("123","Product A",10);
+        const product2 = new Product("567","Product B",12);
+
+        const repository = new ProductRepository();
+        await repository.create(product);
+        await repository.create(product2);
+
+        const response = await request(app)
+            .get("/product")
+
+            expect(response.status).toBe(200);
+            expect(response.body.products.length).toBe(2)
+            expect(response.body.products[0].id).toBe("123")
+            expect(response.body.products[1].id).toBe("567")
+
+        const listResponseXML = await request(app)
+            .get("/product")
+            .set("Accept","Application/xml")
+            .send();
+
+            expect(listResponseXML.status).toBe(200);
+            expect(listResponseXML.text).toContain(`<?xml version="1.0" encoding="UTF-8"?>`);
+            expect(listResponseXML.text).toContain(`<products>`);
+            expect(listResponseXML.text).toContain(`<product>`);
+            expect(listResponseXML.text).toContain(`<name>Product A</name>`);
+    });
 })
